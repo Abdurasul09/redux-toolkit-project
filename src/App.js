86d73a69d3a341c7d782905cce3432c6bc8464d6
Abdurasul <ahmedovabdurasul19@gmail.com>
2023-03-16 09:02:48 +0100

    routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import {Routes, Route} from 'react-router-dom'
+import { Main, Login, Register } from './components';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div>
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
