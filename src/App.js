c19a2be5113d2e3316d6cdf40016037543d014a4
Abdurasul <ahmedovabdurasul19@gmail.com>
2023-03-16 09:47:54 +0100

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import {Routes, Route} from 'react-router-dom'
-import { Main, Login, Register } from './components';
+import { Routes, Route } from "react-router-dom";
+// import { Main, Login, Register } from "./components";
+import {Main} from './components/Main'
+import {Login} from './components/Login'
+import {Register} from './components/Register'
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Routes>
-        <Route path='/' element={<Main/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
