f21c36f852a5332df05ca1e2bf080286e040796e
Abdurasul <ahmedovabdurasul19@gmail.com>
2023-03-16 10:54:26 +0100

    register page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Input } from "../ui/Input";
 
 export const Login = () => {
-	return (
-		<div>Login</div>
-	)
-}
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  return (
+    <div className="flex items-center justify-center h-[80vh]">
+      <div className="border w-96 p-6 rounded">
+        <h3 className="text-zinc-900 text-center pb-6 font-semibold text-lg">
+          Login
+        </h3>
+        <form className="max-w-md mx-auto">
+          <div className="mb-4">
+            <Input label={"Email"} state={email} setState={setEmail} />
+          </div>
+          <div className="mb-4">
+            <Input label={"Password"} state={password} setState={setPassword} />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-zinc-700 hover:bg-zinc-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Login
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="/"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
